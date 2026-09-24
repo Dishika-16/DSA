ee28ad484b58bf62122b53f94554ff978da8e7ea
Dishika Rajawat <2k24.csaiml1b.2411498@gmail.com>
2026-09-24 18:45:29 +0530

    Time: 2 ms (72.28%), Space: 15 MB (36.25%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        int n = path.length();
+
+        for(int i = 0; i < n; i++) {
+
+            if(path[i] == '/') {
+                continue;
+            }
+
+            string temp = "";
+
+            while(i < n && path[i] != '/') {
+                temp += path[i];
+                i++;
+            }
+
+            if(temp == ".") {
+                continue;
+            }
+            else if(temp == "..") {
+                if(!st.empty()) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push(temp);
+            }
+        }
+
+        string ans = "";
+
+        while(!st.empty()) {
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+
+        if(ans == "")
+            return "/";
+
+        return ans;
+    }
+};
